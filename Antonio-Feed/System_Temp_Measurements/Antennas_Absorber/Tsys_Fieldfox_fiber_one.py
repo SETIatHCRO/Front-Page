421eb538b19962fa70edbe1f75ca852de47b624a
Alexander Pollak <Alexander.Pollak.87@gmail.com>
2022-09-15 11:47:39 -0700

    added Tsys 5e

--- Antonio-Feed/System_Temp_Measurements/Antennas_Absorber/Tsys_Fieldfox_fiber_one.py
+++ Antonio-Feed/System_Temp_Measurements/Antennas_Absorber/Tsys_Fieldfox_fiber_one.py
@@ -2,10 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-t_hot = 280
-t_cold = 16
-ant = '3c'
-nm = '1'
+t_hot = 292
+t_cold = 12
+ant = '5e'
+nm = '2'
 year='2022'
 
 cold_l_ing = np.zeros((801), dtype=float)
