38674b265b1ca5316c82ec6c6f754c03cc7f477a
Sofysicist <8907742+Sofysicist@users.noreply.github.com>
2023-04-23 17:36:31 -0700

    Updated to use astropy (Sofia's edit)

--- Beamforming-Utility-Scripts/beamformer_functions.py
+++ Beamforming-Utility-Scripts/beamformer_functions.py
@@ -1,24 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import astropy.units as u
+import astropy.constants as const
+from astropy.coordinates import Angle
 
 def primary_beam_plot(center_freq1, center_freq2):
     # Primary beam diameter equation
     def primary_beam_diameter(center_freq):
         # Constants
-        c = 299792458  # Speed of light [m/s]
-        d = 25  # Antenna diameter [m]
-        lambda_ = c / (center_freq * 1e9)  # Wavelength [m]
-        # Primary beam diameter [arcsec]
-        return 1.22 * lambda_ * (180 / np.pi) * (1 / d) * 3600
+        c = const.c  # Speed of light
+        d = 6.1 * u.m  # Antenna diameter
+        center_freq = center_freq * u.GHz # Assume user input in GHz
+        lambda_ = c / center_freq # Wavelength
+        # Primary beam diameter
+        pb_diam = Angle((1.22 * lambda_ / d) * u.radian)
+        return pb_diam.degree
 
-    # Generate theta values (in arcsec) for the primary beam circle
-    theta = np.linspace(0, 360, 1000)
-
-    # Calculate primary beam diameters (in arcsec) for both center frequencies
+    # Calculate primary beam diameters (in degrees) for both center frequencies
     pb_diam1 = primary_beam_diameter(center_freq1)
     pb_diam2 = primary_beam_diameter(center_freq2)
 
-    # Calculate radius (in arcsec) for both primary beams
+    # Calculate radius (in degrees) for both primary beams
     pb_radius1 = pb_diam1 / 2
     pb_radius2 = pb_diam2 / 2
 
@@ -36,11 +38,12 @@ def primary_beam_plot(center_freq1, center_freq2):
     ax.set_ylim([-max(pb_radius1, pb_radius2), max(pb_radius1, pb_radius2)])
     ax.set_aspect('equal')
 
+    # Add units to the plot
+    ax.set_xlabel('Distance from primary beam center (degrees)')
+    ax.set_ylabel('Distance from primary beam center (degrees)')
+
     # Add legend to the plot
     ax.legend()
 
     # Show the plot
     plt.show()
-
-
-primary_beam_plot(1.4, 3.0)
