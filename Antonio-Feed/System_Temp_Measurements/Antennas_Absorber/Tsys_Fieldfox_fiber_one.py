c2de7ab75c1cadd1632b2b61636eb859f69b4281
Alexander Pollak <Alexander.Pollak.87@gmail.com>
2025-07-18 12:03:28 -0700

    did HC measurement on 5B

--- Antonio-Feed/System_Temp_Measurements/Antennas_Absorber/Tsys_Fieldfox_fiber_one.py
+++ Antonio-Feed/System_Temp_Measurements/Antennas_Absorber/Tsys_Fieldfox_fiber_one.py
@@ -2,10 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-t_hot = 285
-t_cold = 16
-ant = '1c'
-nm = '2'
+t_hot = 302
+t_cold = 14
+ant = '5b'
+nm = '1'
 year='2025'
 
 cold_l_ing = np.zeros((801), dtype=float)
