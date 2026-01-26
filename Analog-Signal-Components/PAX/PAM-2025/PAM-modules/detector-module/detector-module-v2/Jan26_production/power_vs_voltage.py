06f5a2603ed4e6f2d2fa833b5b0a668202184930
Jay Frothingham <jfrothingham@seti.org>
2026-01-26 09:42:24 -0800

    p-v curve detector module 002
    q
    exit

--- Analog-Signal-Components/PAX/PAM-2025/PAM-modules/detector-module/detector-module-v2/Jan26_production/power_vs_voltage.py
+++ Analog-Signal-Components/PAX/PAM-2025/PAM-modules/detector-module/detector-module-v2/Jan26_production/power_vs_voltage.py
@@ -13,7 +13,7 @@ params = {'axes.labelsize': 20,'axes.linewidth': 1.5, 'legend.fontsize': 16,'leg
     'ytick.minor.pad': 10,'ytick.minor.size': 10,'ytick.minor.visible': True,'ytick.minor.width': 1,
     'ytick.right': True, 'figure.figsize' : (15, 12)}
 rcParams.update(params)
-module = 'module000'
+module = 'module002'
 
 df1 = pd.read_excel(f'pv_{module}.xlsx', sheet_name='Sheet 1')  # optional sheet_name
 
