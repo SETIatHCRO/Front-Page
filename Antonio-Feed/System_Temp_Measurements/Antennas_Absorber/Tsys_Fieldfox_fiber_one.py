0a0a634528e760c6038b2a584c1d0d0e6b8572a0
Jay Frothingham <jfrothingham@seti.org>
2026-06-02 13:01:26 -0700

    hot/cold measurements 2a,2b,2h,2l,2m

--- Antonio-Feed/System_Temp_Measurements/Antennas_Absorber/Tsys_Fieldfox_fiber_one.py
+++ Antonio-Feed/System_Temp_Measurements/Antennas_Absorber/Tsys_Fieldfox_fiber_one.py
@@ -2,10 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-t_hot = 290
+t_hot = 299
 t_cold = 15
-ant = '5e'
-nm = '6'
+ant = '2b'
+nm = '2'
 year='2026'
 
 cold_l_ing = np.zeros((801), dtype=float)
