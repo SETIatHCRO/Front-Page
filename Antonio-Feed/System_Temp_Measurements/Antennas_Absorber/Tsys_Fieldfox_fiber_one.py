1e214c0059efa4140604ef8867a771cb9763ce2b
Jay Frothingham <jfrothingham@seti.org>
2026-06-03 15:43:19 -0700

    hot/cold plots 1g

--- Antonio-Feed/System_Temp_Measurements/Antennas_Absorber/Tsys_Fieldfox_fiber_one.py
+++ Antonio-Feed/System_Temp_Measurements/Antennas_Absorber/Tsys_Fieldfox_fiber_one.py
@@ -2,10 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-t_hot = 305
+t_hot = 300
 t_cold = 15
-ant = '2e'
-nm = '7'
+ant = '1g'
+nm = '2'
 year='2026'
 
 cold_l_ing = np.zeros((801), dtype=float)
