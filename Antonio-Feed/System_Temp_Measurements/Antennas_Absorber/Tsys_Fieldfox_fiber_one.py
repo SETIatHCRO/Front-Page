0ed7c22eb37587a7a4ef9051dbe6f26b8573c3ef
Jay Frothingham <jfrothingham@seti.org>
2026-07-28 16:09:09 -0700

    tsys 1e,1h

--- Antonio-Feed/System_Temp_Measurements/Antennas_Absorber/Tsys_Fieldfox_fiber_one.py
+++ Antonio-Feed/System_Temp_Measurements/Antennas_Absorber/Tsys_Fieldfox_fiber_one.py
@@ -2,10 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-t_hot = 303
+t_hot = 307
 t_cold = 14
-ant = '2k'
-nm = '1'
+ant = '1h'
+nm = '2'
 year='2026'
 
 cold_l_ing = np.zeros((801), dtype=float)
